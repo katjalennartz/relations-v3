a2d92ab25caf270ff2a79ae2fc145e065d623ae1
Katja <47885564+katjalennartz@users.noreply.github.com>
2022-10-03 20:49:07 +0200

    Add files via upload

---
+++ upload/inc/languages/deutsch_du/admin/relations.lang.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Relations 1.0 Main Language File
+ */
+
+ //ACP
+ //INFOS
+$l['relations_title'] = "Risuenas Relations Plugin";
+$l['relations_descr'] = "Beziehungen zu anderen Charakteren anlegen. Mit Kategorien und Unterkategorien.";
+
+$l['relations_website'] = "https://github.com/katjalennartz";
+$l['relations_author'] = "risuena";
+//SETTINGS
+$l['relations_settings_descr'] = "Einstellungen für Risuenas Relations Plugin";
+
+$l['relations_settings_alertConfirmTitle'] = "Relations Bestätigung";
+$l['relations_settings_alertConfirmDescr'] = "Müssen Anfragen anderer Charaktere bestätigt werden?";
+
+$l['relations_settings_alertAlertTitle'] = "Relations Alerts";
+$l['relations_settings_alertAlertDescr'] = "Sollen Alerts überhaupt benutzt werden? Das MyAlerts Plugin muss dafür installiert sein. Ansonsten bitte auch die Bestätigung auf nein stellen.";
+
+$l['relations_settings_alertDeleteTitle'] = "Relations Löschung";
+$l['relations_settings_alertDeleteDescr'] = "Soll der User per Alert informiert werden, wenn eine Relation zu ihm gelöscht wird?";
+
+$l['relations_settings_guestAvaTitle'] = "Gäste und Avatare";
+$l['relations_settings_guestAvaDescr'] = "Dürfen Gäste Avatare sehen?";
+
+$l['relations_settings_imgWidthTitle'] = "Breite der Avtare";
+$l['relations_settings_imgWidthDescr'] = "Wie breit sollen die Avatare im Profil dargestellt werden?";
+
+$l['relations_settings_npcTitle'] = "NPCs";
+$l['relations_settings_npcDescr'] = "Dürfen NPCs eingetragen werden?";
+
+$l['relations_settings_npcImgTitle'] = "NPCs Bilder";
+$l['relations_settings_npcImgDescr'] = "Dürfen für die NPCs Bilder eingetragen werden?";
+
+$l['relations_settings_mycodeTitle'] = "MyCode";
+$l['relations_settings_mycodeDescr'] = "Darf in der Beschreibung der Relation MyCode verwendet werden?";
+
+$l['relations_settings_htmlTitle'] = "HTML";
+$l['relations_settings_htmlDescr'] = "Darf in der Beschreibung der Relation html verwendet werden?";
---
+++ upload/inc/languages/deutsch_du/relations.lang.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Relations 1.0 Main Language File
+ */
+
+ //ACP
+ //INFOS
+$l['relations_ucpnav'] = "Relations";
+$l['myalerts_setting_relation_request'] = "Relations: Bei einer neuen Anfrage.";
+$l['relation_request'] = "Du hast eine Relationanfrage von {1}. Bestätige sie in deinem UCP.";
+
+$l['myalerts_setting_relation_reminder'] = "Relations: Alert wenn ein User dich an eine Anfrage erinnern möchte.";
+$l['relation_reminder'] = "{1} hat dir schon vor einer Weile eine Anfrage geschickt.";
+
+$l['myalerts_setting_relation_delete'] = "Relations: Alert, wenn eine Relation zu dir gelöscht wird.";
+$l['relation_delete'] = "{1} hat eine Rela zu dir gelöscht.";
+
+$l['myalerts_setting_relation_confirm'] = "Relations: Alert, wenn eine Relationsanfrage von dir bestätigt wurde.";
+$l['relation_confirm'] = "{1} hat deine Relationsanfrage bestätigt.";
+
+$l['myalerts_setting_relation_deny'] = "Relations: Alert, wenn eine Relationsanfrage von dir abgelehnt wurde.";
+$l['relation_deny'] = "{1} hat deine Relationsanfrage abgelehnt.";
