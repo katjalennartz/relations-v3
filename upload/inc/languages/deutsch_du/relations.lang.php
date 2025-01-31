224c5c068dd96664f7991a37fb64dee75c9026f2
Katja <47885564+katjalennartz@users.noreply.github.com>
2025-01-31 21:12:54 +0100

    Add files via upload

--- upload/inc/languages/deutsch_du/relations.lang.php
+++ upload/inc/languages/deutsch_du/relations.lang.php
@@ -7,7 +7,7 @@
  //INFOS
 $l['relations_ucpnav'] = "Relations";
 $l['myalerts_setting_relation_request'] = "Relations: Bei einer neuen Anfrage.";
-$l['relation_request'] = "Du hast eine Relationanfrage von {1}. Bestätige sie in deinem UCP.";
+$l['relation_request'] = "Du hast eine Relationanfrage von {1}. Bestätige wenn nötig oder schau sie in deinem UCP an.";
 
 $l['myalerts_setting_relation_reminder'] = "Relations: Alert wenn ein User dich an eine Anfrage erinnern möchte.";
 $l['relation_reminder'] = "{1} hat dir schon vor einer Weile eine Anfrage geschickt.";
@@ -19,4 +19,7 @@ $l['myalerts_setting_relation_confirm'] = "Relations: Alert, wenn eine Relations
 $l['relation_confirm'] = "{1} hat deine Relationsanfrage bestätigt.";
 
 $l['myalerts_setting_relation_deny'] = "Relations: Alert, wenn eine Relationsanfrage von dir abgelehnt wurde.";
-$l['relation_deny'] = "{1} hat deine Relationsanfrage abgelehnt.";
+$l['relation_deny'] = "{1} hat deine Relationsanfrage abgelehnt.<br><div style='max-height:40px; overflow:auto;'>{4}</div>";
+
+$l['relation_nocats'] = "Du hast noch keine Kategorien in deinem Profil angelegt. Bitte tu dies zuerst. Dann kannst du Anfragen an andere Charaktere schicken.";
+
